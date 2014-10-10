a69c8a250fc52e7c3a54e8d65147cfeb6a9dcebd
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-10 13:30:41 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -11,7 +11,19 @@
   mkdir /dspace
   chown dspace /dspace
   
-   a=$(cat /etc/tomcat7/server.xml | grep -n "</Host>"| cut -d : -f 1 )
- 
-   sed -i '$ar /tmp/dspace_tomcat7.conf' /etc/tomcat7/server.xml
-   
+  a=$(cat /etc/tomcat7/server.xml | grep -n "</Host>"| cut -d : -f 1 )
+  sed -i "$((a-1))r /tmp/dspace_tomcat7.conf" /etc/tomcat7/server.xml
+  
+  mkdir /build
+  chmod -R 770 /build
+  cd /build
+  wget http://sourceforge.net/projects/dspace/files/DSpace%20Stable/4.2/dspace-4.2-src-release.tar.gz
+  tar -zxf dspace-4.2-src-release.tar.gz
+  rm dspace-4.2-src-release.tar.gz
+  cd /build/dspace-4.2-src-release
+  mvn -U package
+  cd dspace/target/dspace-4.1-build
+  ant fresh_install
+  chown tomcat7:tomcat7 /dspace -R
+  #this need some help for no interractive
+  /dspace/bin/dspace create-administrator 
