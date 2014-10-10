77c764ccf59f8c272e0dbd7c0b6fdb3063d3132e
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-09 22:43:50 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,6 +8,21 @@ if [ -f /etc/configured ]; then
 else
       #code that need to run only one time ....
         
+         POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
+        POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
+
+       /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+              <<< "UPDATE pg_database SET encoding = pg_char_to_encoding('UTF8') WHERE datname = 'template1'" &>/dev/null
+        /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+                  <<< "CREATE USER dspace WITH SUPERUSER;" &>/dev/null
+        /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+                <<< "ALTER USER dspace WITH PASSWORD 'dspace';" &>/dev/null
+  
+        sed -i 's/ssl = true/ssl = false/' /etc/postgresql/9.3/main/postgresql.conf
+        
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
