77a828d63f559c2228af200afc63d43d2d946312
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-10 00:24:52 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -4,3 +4,9 @@
 #reason of this script is that dockerfile only execute one command at the time but we need sometimes at the moment we create 
 #the docker image to run more that one software for expecified configuration like when you need mysql running to chnage or create
 #database for the container ...
+
+  echo "local all dspace md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+  useradd -m dspace
+  echo "admin:admin"|passwd dspace # need to fix
+  mkdir /dspace
+  chown dspace /dspace
