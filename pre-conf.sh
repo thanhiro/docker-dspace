40d3018a11c2b60b2b6468368b684f22d53df38f
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-10 20:44:18 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -8,7 +8,7 @@
   # need to install maven3
   wget http://ppa.launchpad.net/natecarlson/maven3/ubuntu/pool/main/m/maven3/maven3_3.2.1-0~ppa1_all.deb
   dpkg -i  maven3_3.2.1-0~ppa1_all.deb
-  ln -s /usr/share/maven3/bin/mvn /usr/bin/maven
+  ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
   
   
   echo "local all dspace md5" >> /etc/postgresql/9.3/main/pg_hba.conf
