752c5f73389f1e531255b864138736ab07b6dcee
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-10 22:43:01 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -46,6 +46,7 @@
   mvn -U package
   cd dspace/target/dspace-4.1-build
   #need database running for this command to work ...
+  /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1
   ant fresh_install
   chown tomcat7:tomcat7 /dspace -R
   #this need some help for no interractive
