3a20263cbcb80a2943e8b0b5a95a3d688eaef0d6
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 10:13:26 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,9 @@ if [ -f /etc/configured ]; then
         echo 'already configured'
 else
         #code that need to run only one time ....
-      
+        # /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1
+        ant fresh_install
+        chown tomcat7:tomcat7 /dspace -R
         #needed for fix problem with ubuntu and cron
         update-locale 
         date > /etc/configured
