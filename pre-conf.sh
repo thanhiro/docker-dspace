114c11c844464437c7121f2ddfc8612aaeb4af2e
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 10:17:11 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -44,15 +44,10 @@
   rm dspace-4.2-src-release.tar.gz
   cd /build/dspace-4.2-src-release
   mvn -U package
-  cd dspace/target/dspace-4.1-build
-  #need database running for this command to work ...
-  /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1
-  ant fresh_install
-  chown tomcat7:tomcat7 /dspace -R
-  #this need some help for no interractive
-  /dspace/bin/dspace create-administrator 
-  #stop database ... after install ... 
   
+  
+  
+ 
   apt-get clean \
   rm -rf /tmp/* /var/tmp/* \
   rm -rf /var/lib/apt/lists/*
