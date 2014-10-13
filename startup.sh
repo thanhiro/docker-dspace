d34d7b3b6565dcb88323f0c0ee4b8c3e16341fc9
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 23:43:49 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -25,7 +25,7 @@ else
                 
         echo "local all dspace md5" >> /etc/postgresql/9.3/main/pg_hba.conf
         
-       /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres -D /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1
+       /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres  /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1 &
 
         sleep 10s
         
