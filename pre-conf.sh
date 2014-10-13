ccd0696a5d6d136a9353851a275c6196cf91205d
angel rodriguez-ricardo <angelrr7702@gmail.com>
2014-10-12 23:47:22 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -29,8 +29,43 @@
         rm dspace-4.2-src-release.tar.gz
         cd /build/dspace-4.2-src-release
         mvn -U package
+        
+    #conf database before build and installation of dspace
+        POSTGRESQL_BIN=/usr/lib/postgresql/9.3/bin/postgres
+        POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.3/main/postgresql.conf
+        
+        /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+              <<< "UPDATE pg_database SET encoding = pg_char_to_encoding('UTF8') WHERE datname = 'template1'" &>/dev/null
+
+        /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+                  <<< "CREATE USER dspace WITH SUPERUSER;" &>/dev/null
+        /sbin/setuser postgres $POSTGRESQL_BIN --single \
+                --config-file=$POSTGRESQL_CONFIG_FILE \
+                <<< "ALTER USER dspace WITH PASSWORD 'dspace';" &>/dev/null
+                
+        echo "local all dspace md5" >> /etc/postgresql/9.3/main/pg_hba.conf
+        
+       /sbin/setuser postgres /usr/lib/postgresql/9.3/bin/postgres  /var/lib/postgresql/9.3/main -c config_file=/etc/postgresql/9.3/main/postgresql.conf >>/var/log/postgresd.log 2>&1 &
+
+        sleep 10s
+        
+        /sbin/setuser dspace createdb -U dspace -E UNICODE dspace 
+        # build dspace and install
+        
+        cd /build/dspace-4.2-src-release/dspace/target/dspace-4.1-build
+      
+        ant fresh_install
+        chown tomcat7:tomcat7 /dspace -R
+        #this need some help for no interractive
+        # /dspace/bin/dspace create-administrator 
+     
+        killall postgres
+        sleep 10s
 
   apt-get clean
+  rm -rf /build
   rm -rf /tmp/* /var/tmp/*
   rm -rf /var/lib/apt/lists/*
   
