e4348bf21078f03c4794226a364b17cb7e96c1bc
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-07-31 23:40:06 -0400

    Update create-admin.sh

--- create-admin.sh
+++ create-admin.sh
@@ -2,8 +2,8 @@
 #need to be run to create admin for the container dspace ..
 
 #stop tomcat daemon to be able to run dspace create-administrator
-  sv stop tomcat7
+  sv stop tomcat8
   /dspace/bin/dspace database clean
   /dspace/bin/dspace create-administrator
-  sv start tomcat7
+  sv start tomcat8
   
